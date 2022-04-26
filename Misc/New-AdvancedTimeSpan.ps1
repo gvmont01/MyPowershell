84c431aa477c86e8c30e788762dff7adbb359d83
Gregory Montoya <68292956+gvmont01@users.noreply.github.com>
2022-04-26 09:34:48 -0700

    new file

---
+++ Misc/New-AdvancedTimeSpan.ps1
@@ -0,0 +1,46 @@
+Function New-AdvancedTimeSpan {
+    param(
+        [datetime]$Start,
+        [datetime]$End
+    )
+
+    begin{
+    $workDays = 0
+    $span     = New-TimeSpan -Start $Start -End $End
+    }
+
+    process{
+        
+        $TheDays    = $span.TotalDays
+        $TheHours   = $span.TotalHours
+        $TheMinutes = $span.TotalMinutes
+        $TheYears   = (New-Object DateTime -ArgumentList $span.ticks).Year -1
+
+        #get workdays  - this is the fastest loop construct!
+        for ($d=$start;$d -le $end;$d=$d.AddDays(1)){
+            if ($d.DayOfWeek -notmatch "Sunday|Saturday") {
+                $workDays++
+            }
+        }
+
+       #proper span format 
+       $Span = '{0} years, {1} days, {2} hours, {3} minutes, {4} seconds' -f $TheYears, $(($span.days) % 365), $span.hours, $span.Minutes, $span.Seconds 
+              
+       [PSCustomObject] @{
+            'Total Minutes'  = $TheMinutes
+            'Total Hours'    = $TheHours
+            'Total Days'     = $TheDays
+            'Total Workdays' = $workDays
+            'Total Weeks'    = $TheDays/7
+            'Total Years'    = $TheYears
+            Span             = $Span
+       }
+       
+    }
+
+}
+
+$Start = Get-Date '12/13/2021'
+$End   = Get-Date
+
+New-AdvancedTimeSpan -Start $Start -End $End

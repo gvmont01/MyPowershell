1f65da9f8565d6704e47e9975034996913d179fb
Gregory Montoya <68292956+gvmont01@users.noreply.github.com>
2022-04-27 10:52:24 -0700

    UPDATE

--- Misc/AD Work/Get-ADDomainControllers.ps1
+++ Misc/AD Work/Get-ADDomainControllers.ps1
@@ -1,3 +1,3 @@
-﻿$results = (Get-ADForest).Domains | ForEach-Object{ Get-ADDomainController -Filter * -Server $_ | Select-Object HostName, IPv4Address, site, IsGlobalCatalog }
+﻿$results = (Get-ADForest).Domains | ForEach-Object { Get-ADDomainController -Filter * -Server $_ | Select-Object HostName, IPv4Address, site, IsGlobalCatalog }
 
 $results | Out-GridView -Title '<domain> domain - All Domain Controllers'

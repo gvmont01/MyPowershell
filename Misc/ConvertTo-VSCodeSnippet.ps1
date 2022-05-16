826107f20c492ced8548bbc8bfe145dd0c89ce89
Gregory Montoya <68292956+gvmont01@users.noreply.github.com>
2022-05-15 20:53:29 -0700

    update

--- Misc/ConvertTo-VSCodeSnippet.ps1
+++ Misc/ConvertTo-VSCodeSnippet.ps1
@@ -1,87 +1,51 @@
 function ConvertTo-VSCodeSnippet {
-    <#PSScriptInfo
-
-.VERSION 0.1.0
-
-.GUID 05c41fde-bd40-4dd3-a72e-12ec14a50676
-
-.AUTHOR Tyler Leonhardt
-
-.COMPANYNAME Tyler Leonhardt
-
-.COPYRIGHT (c) Tyler Leonhardt
-
-.TAGS snippets vscode converter
-
-.LICENSEURI https://github.com/PowerShell/PowerShell/blob/master/LICENSE.txt
-
-.PROJECTURI https://gist.github.com/tylerl0706/3d1618572f7a424dab812e9cce5e3a15
-
-.ICONURI https://gist.github.com/tylerl0706/3d1618572f7a424dab812e9cce5e3a15#file-license-txt
-
-.EXTERNALMODULEDEPENDENCIES
-
-.REQUIREDSCRIPTS
-
-.EXTERNALSCRIPTDEPENDENCIES
-
-.RELEASENOTES
-# 0.1.0
-
-Initial Release
-
-
-.PRIVATEDATA
-
-#>
-
     <#
-.SYNOPSIS
-A PowerShell script to help create VSCode snippets.
+    .SYNOPSIS
+    A PowerShell script to help create VSCode snippets.
 
-.DESCRIPTION
-A PowerShell script to help create VSCode snippets. For more information on snippets, please see: https://code.visualstudio.com/docs/editor/userdefinedsnippets
+    .DESCRIPTION
+    A PowerShell script to help create VSCode snippets. For more information on snippets, please see: https://code.visualstudio.com/docs/editor/userdefinedsnippets
 
-.PARAMETER Name
-The name of the snippet.
+    .PARAMETER Name
+    The name of the snippet.
 
-.PARAMETER Body
-The body of the snippet. This is the actual content.
+    .PARAMETER Body
+    The body of the snippet. This is the actual content.
 
-.PARAMETER Prefix
-The prefix used when selecting the snippet in intellisense.
+    .PARAMETER Prefix
+    The prefix used when selecting the snippet in intellisense.
 
-.PARAMETER Scope
-A list of language names to which this snippet applies. If left blank, it will apply to all languages.
+    .PARAMETER Scope
+    A list of language names to which this snippet applies. If left blank, it will apply to all languages.
 
-.PARAMETER Description
-The snippet description.
+    .PARAMETER Description
+    The snippet description.
 
-.PARAMETER IndentationType
-The indentation you want to use. Options are: Tabs, 2Spaces, 4Spaces. 4Spaces is the default.
+    .PARAMETER IndentationType
+    The indentation you want to use. Options are: Tabs, 2Spaces, 4Spaces. 4Spaces is the default.
 
-.EXAMPLE
-$body = "
-Get-Process
-$hello = 'World'
-$hello
-"
-# Take a string and turn it into a snippet
-./ConvertTo-VSCodeSnippet.ps1 -Name Foo -Body $body -Scope 'powershell'
+    .EXAMPLE
+    $body = "
+    Get-Process
+    $hello = 'World'
+    $hello
+    "
+    # Take a string and turn it into a snippet
+    ./ConvertTo-VSCodeSnippet.ps1 -Name Foo -Body $body -Scope 'powershell'
 
-.EXAMPLE
-# Without Scope, this snippet will apply to all languages
-./ConvertTo-VSCodeSnippet.ps1 -Name Foo -Body $body
+    .EXAMPLE
+    # Without Scope, this snippet will apply to all languages
+    ./ConvertTo-VSCodeSnippet.ps1 -Name Foo -Body $body
 
-.EXAMPLE
-# Body is also accepted from the pipeline
-$body | ./ConvertTo-VSCodeSnippet.ps1 -Name Foo
+    .EXAMPLE
+    # Body is also accepted from the pipeline
+    $body | ./ConvertTo-VSCodeSnippet.ps1 -Name Foo
 
-.EXAMPLE
-# Full invocation
-./ConvertTo-VSCodeSnippet.ps1 -Name Foo -Body content -Scope 'powershell','javascript' -Prefix foo -Description 'The best'
+    .EXAMPLE
+    # Full invocation
+    ./ConvertTo-VSCodeSnippet.ps1 -Name Foo -Body content -Scope 'powershell','javascript' -Prefix foo -Description 'The best'
 
-#>
+    #>
     [CmdletBinding()]
     param(
         [Parameter(Mandatory = $true)]
